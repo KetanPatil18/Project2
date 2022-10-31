7220b601b3808cea27bf6df7e772601fdbd10f9a
KetanPatil18 <ketan.patil1304@gmail.com>
2022-10-31 18:32:47 +0530

    Calculator and Test Committed

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.abc.miscellaneous;
+
+public class Calculator {
+int a=10;
+int b=20;
+int c;
+public int sum()
+{ 
+        c=a+b;  
+	return c;
+}
+public int div()
+{ 
+        c=b-a;  
+	return c;
+}
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+package com.abc.miscellaneous;
+
+public class Test {
+public static void main(String[] args) {
+	Calculator c=new Calculator();
+	int sum=c.sum();
+	System.out.println("Sum is: "+sum);
+}
+}
